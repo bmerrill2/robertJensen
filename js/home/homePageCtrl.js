8d41df1c5e83f73b6d563b4c5e6d08cea6fe706d
bmerrill2 <bradmerrill@eighty1designs.com>
2016-12-20 01:15:43 -0700

    added correct carousels and added modal functionality

--- js/home/homePageCtrl.js
+++ js/home/homePageCtrl.js
@@ -1,5 +1,20 @@
 var app = angular.module('robertJensen')
 
 app.controller('homePageCtrl', ['$scope', function ($scope) {
+  
+  $(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
+});
+  
+  $('.owl-carousel').owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    margin:10,
+    nav:false,
+    responsiveClass:true
+})
 
 }])
