15ea0bbd845a1370afca2f94429bf7fa6a58e0b2
bmerrill2 <bradmerrill@eighty1designs.com>
2016-12-21 01:08:53 -0700

    Updated ties and squares page with pictures, carousels and modals

--- js/pocketSquares/pocketSquaresPageCtrl.js
+++ js/pocketSquares/pocketSquaresPageCtrl.js
@@ -1,5 +1,24 @@
 var app = angular.module('robertJensen')
 
 app.controller('pocketSquaresPageCtrl', ['$scope', function ($scope) {
+ 
+  $(document).ready(function () {
+    $('.owl-carousel').owlCarousel();
+  });
+
+  $('.owl-carousel').owlCarousel({
+    items: 4,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    margin: 10,
+    nav: true,
+    navText: [
+    "<i class='fa fa-arrow-circle-left'></i>",
+    "<i class='fa fa-arrow-circle-right'></i>"
+  ],
+    responsiveClass: true
+  })  
 
 }])
